0b6b214bf04cfd4f2342d3e35b0f426c1541490f
Tomer Yanko <tmryanko@gmail.com>
2021-12-08 13:10:55 +0200

    appBar

--- layout/Header/index.jsx
+++ layout/Header/index.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <AppBar>
+      <Container>
+        <Toolbar>
+          <Typography>LOGO</Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Button sx={{ my: 2, color: 'white', display: 'block' }}>dd</Button>
+            <Button sx={{ my: 2, color: 'white', display: 'block' }}>ee</Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Header;
